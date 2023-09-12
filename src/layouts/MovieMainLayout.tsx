010980244980bd04091709af189c8b4bdc96348c
HaaaamBurger <gemasclashes@gmail.com>
2023-09-12 18:58:20 +0300

    #

--- src/layouts/MovieMainLayout.tsx
+++ src/layouts/MovieMainLayout.tsx
@@ -3,8 +3,20 @@ import React from 'react';
 import {Header} from "../components";
 import {Outlet} from "react-router-dom";
 import css from './mainLayout.module.css';
+import {useAppSelector} from "../hooks";
 
 const MovieMainLayout = () => {
+    const {status} = useAppSelector(state => state.themeReducer);
+    const body = document.getElementsByTagName('body')[0];
+
+    if (!status) {
+        body.classList.add(css.themeSwitcher)
+    }
+    else {
+        body.classList.remove(css.themeSwitcher)
+    }
+
+
     return (
         <div className={css.mainLayoutWrapper}>
             <Header/>
